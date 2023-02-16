aa5332dade6b9662ac8a992479c77c814d788903
BILLYCHARMANT <billycharmant541@gmail.com>
2023-02-16 15:19:30 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- 	* main - prints the size of various typesa based on
- 	* the computer it is compiled and run on..
- 	* Return: Always 0.
- 	*/
+	* main - prints the size of various typesa based on
+	* the computer it is compiled and run on..
+	* Return: Always 0.
+	*/
 
 int main(void)
 
