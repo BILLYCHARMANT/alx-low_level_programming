be42a20b929ab1a5cb5e750bc4f4e2d375e35553
BILLYCHARMANT <billycharmant541@gmail.com>
2023-02-21 09:52:53 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
  * main - prints _putchar, fllowd by a new linees
  * Return: Alwys 0 (Success)
  */
+
 int main(void)
+
 {
-	char billy[] = "_putchar";
-
-	int a;
-
-	for (a = 0; a < 8; a++)
-	{
-		_putchar(billy[a]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
