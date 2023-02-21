2f4a08b7b5da72beba69256b155d1417603256e3
BILLYCHARMANT <billycharmant541@gmail.com>
2023-02-21 08:36:47 -0800

    0-purchase

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "Charmant.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *Bl = "Charmant";
+
+	while (*Bl)
+	{
+		_putchar(*Bl);
+		Bl++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
