cebfa018e9caeeb315bdc2a64499ec97eb49f957
BILLYCHARMANT <billycharmant541@gmail.com>
2023-04-09 14:06:03 -0700

    get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_endianess - return the endianess of the machine
+ * Return: 0 for big endian, 1 for little endian
+ */
+int get_endianness(void)
+{
+	int n;
+
+	n = get_endianness();
+
+	if (n != 0)
+	{
+	return (0);
+	}
+	else
+	{
+	return (1);
+	}
+}
