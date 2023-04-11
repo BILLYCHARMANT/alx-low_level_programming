b014e88f05d3dff43afab55fc8cd5d50d51c0bdf
BILLYCHARMANT <billycharmant541@gmail.com>
2023-04-11 07:06:07 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int a;
+
+	n = scanf("%lu input any number", a);
+	printf("%lu binary of n:", n);
+}
